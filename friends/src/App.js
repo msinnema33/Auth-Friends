02fc8fd11bd05ff2f374d161267848036322cc42
Mike Sinnema <msinnema33@gmail.com>
2020-02-24 21:40:10 -0700

    updated app to include addfriends privateroute

--- friends/src/App.js
+++ friends/src/App.js
@@ -18,11 +18,15 @@ function App() {
             <Link to='/login'>Log In</Link>
           </li>
           <li>
-            <Link to='FriendsList'>Friends List</Link>
+            <Link to='/FriendsList'>Friends List</Link>
+          </li>
+          <li>
+          <Link to='/AddFriends'>Add Friends</Link>
           </li>
         </ul>
         <Switch>
-          <PrivateRoute path='FriendsList' component={FriendsList}/>
+          <PrivateRoute path='/FriendsList' component={FriendsList}/>
+          <PrivateRoute path='/AddFriends' component={AddFriends}/>
           <Route path='/login' component={Login}/>
         </Switch>
       </div>
